c4b263547178d11f3691cdcd387335917b4e1d59
Poorna Ranasinghe <onlinepoorna@gmail.com>
2023-02-24 14:31:07 +0530

    HTTP CODES 1.0

---
+++ test-folder/script.js
@@ -0,0 +1,5 @@
+const http = require("http-status-responser");
+
+console.log("====================================");
+console.log(http.ACCEPTED);
+console.log("====================================");
